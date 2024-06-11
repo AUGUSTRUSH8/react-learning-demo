d153c135a81a8ea12b2c332195fde00b811006c9
rush <643098432@qq.com>
2024-06-11 11:48:56 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import BlogPost from './components/BlogPost';
+
+function App() {
+  return (
+    <div>
+      <h1>My Blog</h1>
+      <BlogPost />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/BlogPost.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function BlogPost() {
+  return (
+    <div>
+      <h2>My First Blog Post</h2>
+      <p>This is the content of my first blog post.</p>
+    </div>
+  );
+}
+
+export default BlogPost;
